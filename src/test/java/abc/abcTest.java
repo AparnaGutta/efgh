cd25a02fa7c8c209726cdf0b68cbde116075ae42
AparnaGutta <anu.aparna419@gmail.com>
2018-04-11 16:10:13 +0530

    first commit by anu

---
+++ src/test/java/abc/abcTest.java
@@ -0,0 +1,29 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class abcTest {
+
+	@Test
+	public void m1()
+	{
+		System.out.println("Hi i am m1 method");
+	}
+	
+	@Test
+	public void m2()
+	{
+		System.out.println("Hi i am m2 method");
+	}
+	@Test
+	public void m3()
+	{
+		System.out.println("Hi i am m3 method");
+	}
+	@Test
+	public void m4()
+	{
+		System.out.println("Hi i am m4 method");
+	}
+
+}
